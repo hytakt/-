912e24d82ab59aa2b174b6efcc08df27c8705357
hytakt <hayate.akutsu04202002@gmail.com>
2023-08-21 18:40:24 +0900

    titleの追加

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts/index');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
