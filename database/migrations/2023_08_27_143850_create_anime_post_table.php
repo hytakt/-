2e5e6860926b803a0d36f4d1928aa2945296c0a3
hytakt <hayate.akutsu04202002@gmail.com>
2023-08-27 20:19:41 +0900

    中間テーブルの作成

---
+++ database/migrations/2023_08_27_143850_create_anime_post_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('anime_post', function (Blueprint $table) {
+            $table->foreignId('anime_id')->constrained('animes');
+            $table->foreignId('post_id')->constrained('posts');
+            $table->primary(['anime_id', 'post_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('anime_post');
+    }
+};
